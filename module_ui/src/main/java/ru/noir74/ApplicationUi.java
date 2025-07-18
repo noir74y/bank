5d668ae9491b6fb7af65c6b6c139b36c031a3f80
noir74y <noir74@yandex.ru>
2025-07-19 00:47:01 +0300

    Initial commit

---
+++ module_ui/src/main/java/ru/noir74/ApplicationUi.java
@@ -0,0 +1,13 @@
+package ru.noir74;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationUi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationUi.class, args);
+	}
+
+}
